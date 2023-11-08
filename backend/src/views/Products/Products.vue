92406bba42f33aaa1b9c292a944338c670172d3c
John Rocha <xjohnfitcodes@gmail.com>
2023-11-08 14:06:08 -0500

    Add new product Modal

--- backend/src/views/Products/Products.vue
+++ backend/src/views/Products/Products.vue
@@ -3,17 +3,26 @@
         <h1 class="text-3xl font-semibold">Products</h1>
         <button
             type="submit"
+            @click="showProductModal"
             class="py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
             Add New Product
         </button>
     </div>
+    <ProductModal v-model="showModal" />
     <ProductsTable />
 </template>
 
 <script setup>
 import ProductsTable from './ProductsTable.vue';
+import ProductModal from './ProductModal.vue';
+import { ref } from "vue";
 
+const showModal = ref(false);
+
+function showProductModal(){
+    showModal.value = true;
+}
 
 </script>
 
