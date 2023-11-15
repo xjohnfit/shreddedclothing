4920f7c8d0e408500791a1061cf8051233586f0b
John Rocha <xjohnfitcodes@gmail.com>
2023-11-15 15:55:29 -0500

    MenuItems CSS updated

--- backend/src/views/Products/ProductsTable.vue
+++ backend/src/views/Products/ProductsTable.vue
@@ -130,7 +130,7 @@
                                 leave-to-class="transform scale-95 opacity-0"
                             >
                                 <MenuItems
-                                    class="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
+                                    class="absolute z-10 right-0 mt-2 w-32 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                                 >
                                     <div class="px-1 py-1">
                                         <MenuItem v-slot="{ active }">
